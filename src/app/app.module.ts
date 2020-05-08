96f0119e195bd9f644c2222453b536ddea476af7
David Condori Quispe <davidcondoriquispe@MacBook-Pro-de-David.local>
2020-05-08 05:45:02 -0500

    modelo para manejar heroes y formulario de edicion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HeroesComponent } from './pages/heroes/heroes.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,4 +1,6 @@
+import { HeroeModel } from './../../models/heroe.model';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-heroe',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
+  heroe = new HeroeModel();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar( form: NgForm ) {
+    console.log(form);
+    console.log(this.heroe);
+
   }
 
 }
